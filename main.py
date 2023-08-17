6902f731fa960f3d421aecc15ba6a046e4d63a70
tiebanluyu <108140288+tiebanluyu@users.noreply.github.com>
2023-08-17 15:58:35 +0800

    先传一次

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
 import turtle as t
-win=t.Screen()
-win.title("华强买瓜")
-win.screensize(960,720)
-turtle=t.Turtle()
-t.tracer(0)
-def juxing(x,y,chang,kuan):
-    turtle.penup()
-    turtle.goto(x,y)
-    turtle.pendown()
-    turtle.left(90)
-    turtle.forward(chang)
-    turtle.right(90)
-    turtle.forward(kuan)
-    turtle.right(90)
-    turtle.forward(chang)
-    turtle.left(90)
-def yuan (x,y,r):
-    turtle.penup()
-    turtle.goto(x,y)
-    turtle.pendown()
-    turtle.circle(r)
-
-juxing(-410,-360,240,200)
-juxing(-10,-360,240,200)
-yuan(-110,)
+import time
+t.bgpic("3.png")
 t.update()
-
-
+time.sleep(1)
+t.bgpic("2.png")
+t.update()
 t.done()
-
-
-
-#win.mainloop()
-
